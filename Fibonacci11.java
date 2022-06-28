2d62ab10eccd5137fcf37bacb7240f9dfbcc1ff2
Nikita Jain <106953552+20niki@users.noreply.github.com>
2022-06-28 14:08:07 +0530

    Add files via upload

---
+++ Fibonacci11.java
@@ -0,0 +1,20 @@
+class Fibonacci11
+	{
+		public static void main(String args[])
+	{  
+ 		int n1=0,n2=1,n3=0,i,count=10;  
+ 		
+  		i=2;
+ 		while(i<=count)
+ 		
+ 		{  
+		System.out.print(n3+" ");  
+  		n3=n1+n2;  
+  		
+  		n1=n2;  
+  		n2=n3; 
+		i++; 
+ 		}  
+
+	}
+	}
